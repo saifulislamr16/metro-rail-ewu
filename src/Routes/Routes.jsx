9d0fa3bf2b9801d56abf9d5db951a9ea5ea05987
saifulislamr16 <saifulislamr16@gmail.com>
2023-05-22 22:21:54 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../Home/Home";
+import MRT from "../MRT/MRT";
+import MapWithForm from "../MapWithForm/MapWithForm";
+import Login from "../component/Login";
+import SignUp from "../component/SignUp";
+import Pass from "../Passs/Pass";
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"/mrt",
+                element:<MRT></MRT>
+            },
+            {
+                path:"/route",
+                element:<MapWithForm></MapWithForm>
+            },
+            {
+                path:"/login",
+                element:<Login></Login>
+            },
+            {
+                path:"/Signup",
+                element:<SignUp></SignUp>
+            },
+            {
+                path:"/pass",
+                element:<Pass></Pass>
+            }
+        ]
+    },
+    {
+        
+    }
+])
+
+export default routes;
